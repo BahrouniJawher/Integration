8edad63c310122f3dba3a66555ab7b5c8fa1fe8e
taekouando <jawher.bahrouni@esprit.tn>
2020-11-05 01:49:40 +0100

    log4j et sonar bugs

--- timesheet-prof (1)/src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ timesheet-prof (1)/src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.services;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
@@ -28,19 +28,19 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	
 	private static final Logger l = LogManager.getLogger(EntrepriseServiceImpl.class);
 	
-	public int ajouterEntreprise(Entreprise entreprise) {
-		l.info("In ajouterEntreprise() : ");
+	public Entreprise ajouterEntreprise(Entreprise entreprise) {
+		l.info("In  ajouterEntreprise : " + entreprise);
 		entrepriseRepoistory.save(entreprise);
 		l.info("Out ajouterEntreprise() without errors.");
-		return entreprise.getId();
+		return entreprise;
 		
 	}
 
-	public int ajouterDepartement(Departement dep) {
+	public Departement ajouterDepartement(Departement dep) {
 		l.info("In ajouterEntreprise() : ");
 		deptRepoistory.save(dep);
 		l.info("Out ajouterEntreprise() without errors.");
-		return dep.getId();
+		return dep;
 	}
 	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
@@ -50,24 +50,26 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 				//Rappel : la classe qui contient mappedBy represente le bout Slave
 				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
 	          	l.info("In affecterDepartementAEntreprise() : ");
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
-				
-				depManagedEntity.setEntreprise(entrepriseManagedEntity);
-				deptRepoistory.save(depManagedEntity);
+	          	Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).orElse(null);
+	          	Departement depManagedEntity = deptRepoistory.findById(depId).orElse(null);
+	          	if(depManagedEntity!=null) {
+	            depManagedEntity.setEntreprise(entrepriseManagedEntity);
+	            deptRepoistory.save(depManagedEntity);
 				l.info("Out affecterDepartementAEntreprise() without errors.");
+	          	}
 		
 	}
 	
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
 		l.info("In getAllDepartementsNamesByEntreprise() : ");
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).orElse(null);
 		List<String> depNames = new ArrayList<>();
+		if(entrepriseManagedEntity!=null){
 		for(Departement dep : entrepriseManagedEntity.getDepartements()){
 			l.debug("Je viens de chercher les departement dep.");
 			depNames.add(dep.getName());
 			
-		}
+		}}
 		l.info("Out getAllDepartementsNamesByEntreprise() without errors.");
 		return depNames;
 	}
@@ -89,7 +91,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 
 	public Entreprise getEntrepriseById(int entrepriseId) {
 		l.info("In getEntrepriseById() : ");
-		Entreprise x =  entrepriseRepoistory.findById(entrepriseId).get();
+		Entreprise x =  entrepriseRepoistory.findById(entrepriseId).orElse(null);
 		l.info("Out getEntrepriseById() without errors.");
 		return x;
 		
